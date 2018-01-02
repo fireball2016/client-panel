65b4f5afd460c937a28e778fc7f1a59e09a8f6c0
fireball2016 <fireball110@gmx.com>
2018-01-01 23:36:44 -0800

    first

---
+++ src/environments/environment.ts
@@ -0,0 +1,16 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDtGOGNBQPSPFEdiqwbsNubur-px8QrB_w",
+    authDomain: "client-panel-app-80c01.firebaseapp.com",
+    databaseURL: "https://client-panel-app-80c01.firebaseio.com",
+    projectId: "client-panel-app-80c01",
+    storageBucket: "client-panel-app-80c01.appspot.com",
+    messagingSenderId: "438632988923"
+  }
+};
