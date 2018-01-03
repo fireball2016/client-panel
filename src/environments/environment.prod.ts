e0cb8a9e0921b6a03807dabe177d016f12b070cd
fireball2016 <fireball110@gmx.com>
2018-01-03 14:30:07 -0800

    final

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDtGOGNBQPSPFEdiqwbsNubur-px8QrB_w",
+    authDomain: "client-panel-app-80c01.firebaseapp.com",
+    databaseURL: "https://client-panel-app-80c01.firebaseio.com",
+    projectId: "client-panel-app-80c01",
+    storageBucket: "client-panel-app-80c01.appspot.com",
+    messagingSenderId: "438632988923"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   firebase: {
     apiKey: "AIzaSyDtGOGNBQPSPFEdiqwbsNubur-px8QrB_w",
     authDomain: "client-panel-app-80c01.firebaseapp.com",
